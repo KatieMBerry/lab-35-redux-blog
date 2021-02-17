0d930fd9dea12df9220b3a1d1a2c23360a963b72
Katie <katieberrym@gmail.com>
2021-02-17 14:56:05 -0800

    added BlogList to App, added useBlogState to provider

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlogForm from '../form/BlogForm';
+import BlogList from '../blogs/BlogList';
 
 export default function App() {
   return (
     <>
       <BlogForm />
+      <BlogList />
     </>
   );
 }
--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useBlogState } from '../../state/blogProvider';
 import Blog from './Blog';
 
 const BlogList = () => {
+    //using state from provider that was dispatched to reducer
+    const blogs = useBlogState();
     //grabs state from provider and returns uls
     const blogElements = blogs.map(blog => {
         <li key={blog.title}>
-            <Blog {...blogPosts} />
+            <Blog {...blogs} />
         </li>
     });
 
