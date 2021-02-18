cfc7a10da9b1eaff93dd22abd0894e5ca946d6e9
Katie <katieberrym@gmail.com>
2021-02-18 14:43:33 -0800

    installed libraries, added store.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
+import store from './store';
 import { BlogsProvider } from './state/BlogsProvider';
 
 render(
